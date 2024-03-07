656747d9c716afef2df3630e16d11d3f61efef7b
Burak Yılmam <61635780+Burakyilmam@users.noreply.github.com>
2024-03-07 22:36:28 +0300

    Add project files.

---
+++ DevExpressXpoDemo/Opening.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DevExpressXpoDemo
+{
+    public partial class Opening : DevExpress.XtraEditors.XtraForm
+    {
+        public Opening()
+        {
+            InitializeComponent();
+        }
+    }
+}
